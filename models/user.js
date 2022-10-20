985486ce2599c4b6e3d067f0196e1e691b465302
MadhaviImashi <madhaviimashi@gmail.com>
2022-10-20 18:33:46 +0530

    user schema created

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+    },
+    type: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
