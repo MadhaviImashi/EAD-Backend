fce2f3ded291eb662972e735b895941ea57feb6b
MadhaviImashi <madhaviimashi@gmail.com>
2022-10-24 11:24:53 +0530

    addUserToQueue API

--- controllers/fuelManagement.js
+++ controllers/fuelManagement.js
@@ -2,8 +2,8 @@ const User = require('../models/user');
 const FuelShed = require('../models/fuelShed');
 
 //method: get station details by stationId
-const getFuelStationDetails = async (req, res) => {
-    const station_id = req.body.id;
+const getFuelStationDetails = async (req, response) => {
+    const station_id = req.body.station_id;
 
     try {
         const fuelShed = await FuelShed.findById(station_id)
@@ -13,7 +13,7 @@ const getFuelStationDetails = async (req, res) => {
             .populate({ path: "Petrol.bikeQueue" })
             .populate({ path: "Petrol.threeWheelerQueue" })
             .exec(() => (err,result)= {});
-        res.status(200).json({
+        response.status(200).json({
             success: true,
             message: "GET station details",
             fuelShed: fuelShed
@@ -25,10 +25,13 @@ const getFuelStationDetails = async (req, res) => {
 }
 
 //method: update fuel details of a particular station
+const updateFuelStationDetails = async(req, res) = {
+    
+}
 
 //method: Search fuel station (get all details of searched StationId)
-const getDetailsOfSearchedFuelStation = async (req, res) => {
-    const searched_station_id = req.body.id;
+const getDetailsOfSearchedFuelStation = async (req, response) => {
+    const searched_station_id = req.body.station_id;
 
     try {
         const fuelShed = await FuelShed.findById(searched_station_id)
@@ -38,7 +41,7 @@ const getDetailsOfSearchedFuelStation = async (req, res) => {
             .populate({ path: "Petrol.bikeQueue" })
             .populate({ path: "Petrol.threeWheelerQueue" })
             .exec(() => (err,result)= {});
-        res.status(200).json({
+        response.status(200).json({
             success: true,
             message: "GET station details",
             fuelShed: fuelShed
@@ -49,7 +52,70 @@ const getDetailsOfSearchedFuelStation = async (req, res) => {
     }
 }
 //method: add user to the correct queue(ex: stationId.petrol.carQueue.push(user))
+const addUserToFuelQueue = async (req, response) => {
+//find the user who is going to join the queue, the station
+    const user_id = req.body.user_id;
+    const station_id = req.body.station_id;
+    let user, station;
+    try {
+        user = await User.findById(user_id);
+        station = await FuelShed.findById(station_id);
+    }
+    catch (error) {
+        console.log(error, 'matching user or station not found');
+    }
 
+//find the correct category to which user belongs to
+    const fuelType = req.body.fuelType; // 'Diesel' or 'Petrol'
+    const vehicalType = req.body.vehicalType // 'bus' / 'threeWheeler' / 'car' / 'bike' 
+    let queueType;
+
+    if (fuelType === 'Diesel') {
+        switch (vehicalType) {
+            case 'bus':
+                queueType = station.Diesel.busQueue;
+                break;
+            case 'threewheeler':
+                queueType = station.Diesel.threeWheelerQueue;
+                break;
+            default:
+                break;
+        }
+    } else if (fuelType === 'Petrol') {
+        switch (vehicalType) {
+            case 'bus':
+                queueType = station.Petrol.busQueue;
+                break;
+            case 'threewheeler':
+                queueType = station.Petrol.threeWheelerQueue;
+                break;
+            case 'bike':
+                queueType = station.Petrol.bikeQueue;
+            default:
+                break;
+        }
+    }
+
+//add the user to the correct queue
+    try {
+        queueType.push(user);
+        station.save()
+            .then((res) => {
+                console.log('user added to queue successfully');
+                response.status(200).json({
+                    success: true,
+                    message: "user added to correct queue successfully",
+                    data: res,
+                })
+                    .catch((err) => {
+                        console.log(err, "couldn't add user to the fuel queue");
+                })
+        })
+    }
+    catch (err) {
+        console.log(err, "couldn't add user to the fuel queue")
+    }
+}
 //method: get the lenght of each queue (lenght of queue array)
 //method: get waiting time of each queue (ex: stationId.petrol.carQueue[0].arrivalTime)
 
@@ -60,6 +126,8 @@ const getDetailsOfSearchedFuelStation = async (req, res) => {
 const all = {
     getFuelStationDetails,
     getDetailsOfSearchedFuelStation,
+    updateFuelStationDetails,
+    addUserToFuelQueue,
 }
 
-module.exports = all
+module.exports = all;
--- routes/FuelManagement.js
+++ routes/FuelManagement.js
@@ -4,5 +4,6 @@ const fuelManager = require("../controllers/fuelManagement");
 
 router.get("/", fuelManager.getFuelStationDetails);
 router.get("/search", fuelManager.getDetailsOfSearchedFuelStation);
-
+router.put("/", fuelManager.updateFuelStationDetails);
+router.post("/add-to-queue", fuelManager.addUserToFuelQueue);
 module.exports = router;
